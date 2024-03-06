20ec297d3724b39226c0e249fd439f1eb2b0cbd5
szynutix-sz <szynutix@protonmail.com>
2024-03-06 15:04:59 +0100

    Beginning chapter 2 again?

---
+++ chapter2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.10
+#
+#
+# This is a test
+# This is a test of magit
+
+
+print("Hello Python World!")
+
+message = "Hello Python Crash Course Readers"
+print(message)
+message = "Message v2"
+print(message)
