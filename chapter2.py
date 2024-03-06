3819b7c6375e53a85268a82bcbcc1e3f9eb64ead
szynutix-sz <szynutix@protonmail.com>
2024-03-06 15:48:00 +0100

    Further

--- chapter2.py
+++ chapter2.py
@@ -6,8 +6,12 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 print(f"Hello, {full_name.title()}")
 
-message = "Languages:\n\tPython\n\tJava\n\tJavaScript    "
+message = "  Languages:\n\tPython\n\tJava\n\tJavaScript    "
 print(message)
 
 message = message.strip()
 print(message)
+
+google_url = "https://www.google.com"
+simple_url = google_url.removeprefix("https://")
+print(simple_url)
