67c73f9aeaf8f6b4d2f1b34597a9d97c969d0484
szynutix-sz <szynutix@protonmail.com>
2024-03-06 15:55:37 +0100

    Further

--- chapter2.py
+++ chapter2.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3.10
 
-first_name = "ada"
-last_name = "lovelace"
+# first_name = "ada"
+# last_name = "lovelace"
 
-full_name = f"{first_name} {last_name}"
-print(f"Hello, {full_name.title()}")
+# full_name = f"{first_name} {last_name}"
+# print(f"Hello, {full_name.title()}")
 
-message = "  Languages:\n\tPython\n\tJava\n\tJavaScript    "
+# message = "  Languages:\n\tPython\n\tJava\n\tJavaScript    "
+# print(message)
+
+# message = message.strip()
+# print(message)
+
+# google_url = "https://www.google.com"
+# simple_url = google_url.removeprefix("https://")
+# print(simple_url)
+
+message = "One of Python's streanghts is its community"
 print(message)
-
-message = message.strip()
-print(message)
-
-google_url = "https://www.google.com"
-simple_url = google_url.removeprefix("https://")
-print(simple_url)
