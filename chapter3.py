05629618d021d7344197783a7b7de5154944b31c
szynutix-sz <szynutix@protonmail.com>
2024-03-10 19:59:01 +0100

    A bit further.

--- chapter3.py
+++ chapter3.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3.12
 #
-list = ['trek', 'specialized', 'gondole', 'redline']
-mess = f"My first bicicle was a {list[0].title()}"
-print(mess)
-list[0] = 'honda'
-mess = f"My first bicicle was a {list[0].title()}"
-print(mess)
-print(list[1])
+# list = ['trek', 'specialized', 'gondole', 'redline']
+# mess = f"My first bicicle was a {list[0].title()}"
+# print(mess)
+# list[0] = 'honda'
+# mess = f"My first bicicle was a {list[0].title()}"
+# print(mess)
+# print(list[1])
+# li = [1,2,3,4]
+# print(li[-3:-1])
+# li.append(5)
 li = [1,2,3,4]
-print(li[-3:-1])
-li.append(5)
-print(li[:6])
+li.insert(-1,0)
+
+print(li)
